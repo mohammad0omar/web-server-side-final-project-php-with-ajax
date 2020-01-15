25978fddf5dde65df3a71b32018ae461fb6612f0
ahmadAtrach <54948072+ahmadAtrach@users.noreply.github.com>
2020-01-15 18:50:43 +0200

    add login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $_SESSION['username']=$_POST['username'];
+    // 86400 = 1 day
+    $_SESSION['password']=$_POST['password'];
+    
+    if(isset($_SESSION['username']))
+        header('location: index.html');
+    
+}
+?>
